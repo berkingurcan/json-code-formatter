093e6644ae57d266e5cabeda6685b6534c9ca70e
berkingurcan <berkingurcan@yandex.com>
2023-07-06 14:44:00 -0300

    api result

--- nextjs-flask/api/index.py
+++ nextjs-flask/api/index.py
@@ -1,6 +1,40 @@
+import sys
+import json
+
 from flask import Flask
 app = Flask(__name__)
 
+def format_rust_code(rust_code):
+    formatted_code = rust_code.strip()
+    return json.dumps(formatted_code)
+
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    app.logger.info('testing info log')
+    print('testing print log', file=sys.stderr)
+    rust_code =  """
+    use anchor_lang::prelude::*;
+    declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+    
+    #[program]
+    pub mod hello_world {
+        use super::*;
+        pub fn hello_world(_ctx: Context<Initialize>) -> Result<()> {
+            msg!("Hello world, from solana smart contract");
+            Ok(())
+        }
+    }
+    #[derive(Accounts)]
+    pub struct Initialize {}
+
+    """
+
+    formatted_code = format_rust_code(rust_code)
+    print(formatted_code)
+    return formatted_code
+
+
+
+
+
+
